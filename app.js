70da83fc101f29b3bb5706448fb64d646eba85d3
Rob <rob.dickerson.dev@gmail.com>
2017-12-04 12:31:45 -0800

    Refactor: Getting ready for tweeter challenge
    end_of_libe

--- app.js
+++ app.js
@@ -3,16 +3,25 @@ var app = express();
 var bodyParser = require('body-parser')
 app.use(bodyParser.json());
 
+
+var tweets = [
+  {text: "Hai dude.", time: new Date().getTime() - 12300},
+  {text: "This is cool.", time: new Date().getTime() - 1000},
+  {text: "What's up?", time: new Date().getTime()},
+];
+
 app.use(express.static(__dirname + '/public'));
 
-app.post('/login', function(req, res){
-    console.log(req.body.username + " " +  req.body.password);
-    if (req.body.username === "brian" && req.body.password === "pass") {
-        res.json(200, {status:"success"});
-    }
-    else {
-        res.json(401, {status:"failure"})
-    }
+app.get('/ajax', function(request, response) {
+  response.type('json');
+  response.end(JSON.stringify({tweets:tweets}));
+});
+
+app.post('/ajax', function(request, response) {
+  var newTweet = {text: request.body.tweet, time: new Date().getTime()};
+  tweets.push(newTweet);
+  response.type('json');
+  response.end(JSON.stringify(newTweet));
 });
 
 var server = app.listen(8080);

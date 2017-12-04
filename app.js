2681b35e1ef3d0be298835accf645bdca301c65a
Rob <rob.dickerson.dev@gmail.com>
2017-12-04 12:21:34 -0800

    Refactor: posting
    end_of_line

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser')
+app.use(bodyParser.json());
 
-app.get('/team/:name', function(req, res){
-    res.setHeader('Content-Type','text/plain');
-    res.send("You picked " + req.params.name);
+app.use(express.static(__dirname + '/public'));
+
+app.post('/login', function(req, res){
+    console.log(req.body.username + " " +  req.body.password);
+    if (req.body.username === "brian" && req.body.password === "pass") {
+        res.json(200, {status:"success"});
+    }
+    else {
+        res.json(401, {status:"failure"})
+    }
 });
 
 var server = app.listen(8080);

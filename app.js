117d6144b1ee13e908c835472ac0b2489de992ed
Rob <rob.dickerson.dev@gmail.com>
2017-12-04 11:56:14 -0800

    Refactor: working with static things
    end_of_line

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
 var express = require('express');
 var app = express();
 
-app.get('/hello.txt', function(req, res){
-    res.send('Hello World');
-});
+app.use(express.static(__dirname + '/public'));
 
-var server = app.listen(8080, function() {
-    console.log('Listening on port 8080');
-});
+var server = app.listen(8080);

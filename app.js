4e8ab37b4b2e3e180de25283f6bbaf061e0cf6a4
Rob <rob.dickerson.dev@gmail.com>
2017-12-04 11:27:39 -0800

    Refactor: setting up express
    end_of_line

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-var http = require('http');
+var express = require('express');
+var app = express();
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello Minnesota\n');
-}).listen(8080, '127.0.0.1');
+app.get('/hello.txt', function(req, res){
+    res.send('Hello World');
+});
 
-console.log('Server running at http://127.0.0.1:8080/');
+var server = app.listen(8080, function() {
+    console.log('Listening on port 8080');
+});
